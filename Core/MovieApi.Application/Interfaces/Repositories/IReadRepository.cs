91cffa729b7a56de3786983996442a736097d4a9
seymaa54 <nalbantseymaa@gmail.com>
2025-10-05 16:25:55 +0300

    refactor: Update repository interfaces and implementations for improved clarity and functionality

--- Core/MovieApi.Application/Interfaces/Repositories/IReadRepository.cs
+++ Core/MovieApi.Application/Interfaces/Repositories/IReadRepository.cs
@@ -1,23 +1,42 @@
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore.Query;
 
-namespace MovieApi.Application.Interfaces;
+namespace MovieApi.Application.Interfaces.Repositories;
 
 public interface IReadRepository<T> where T : class, new()
 {
-    Task<IList<T>> GetAllAsync(Expression<Func<T, bool>>? predicate = null,
-     Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
-     Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-     bool enableTracking = false);
+    /// Tüm kayıtları getirir. Filtre, ilişkili tablolar, sıralama ve izleme seçenekleri ile.
+    Task<IList<T>> GetAllAsync(
+      Expression<Func<T, bool>>? predicate = null,
+      Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+      Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+      bool enableTracking = false);
 
-    Task<T> GetAsync(Expression<Func<T, bool>> predicate,
-     Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
-    bool enableTracking = false);
+    /// Sayfalama ile kayıtları getirir. Filtre, ilişkili tablolar, sıralama ve izleme seçenekleri ile.
+    Task<IList<T>> GetAllByPagingAsync(
+      Expression<Func<T, bool>>? predicate = null,
+      Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+      Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+      bool enableTracking = false,
+      int currentPage = 1,
+      int pageSize = 3);
 
-    IQueryable<T> Find(Expression<Func<T, bool>> predicate,
-    bool enableTracking = false);
+    /// Tek bir kaydı getirir. Filtre, ilişkili tablolar ve izleme seçeneği ile.
+    Task<T> GetAsync(
+      Expression<Func<T, bool>> predicate,
+      Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+      bool enableTracking = false);
 
-    Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    /// Belirtilen filtreye göre sorgu oluşturur. İzleme seçeneği ile.
+    ///IQueryable: C# içinde sorgulanabilir (queryable) nesne demektir.
+    IQueryable<T> Find(
+        Expression<Func<T, bool>> predicate,
+        bool enableTracking = false);
+
+    /// Kayıt sayısını getirir. Filtre ile.
+    Task<int> CountAsync(
+        Expression<Func<T, bool>>? predicate = null);
+
 
     /*
     Task : Metotların asenkron olarak çalışmasını sağlar.
@@ -27,6 +46,6 @@ public interface IReadRepository<T> where T : class, new()
     : İlişkili tabloları (navigation property) sorguya dahil etmek için kullanılır. q=> q.Include(x => x.Actors)
     Func<IQueryable<T>, IOrderedQueryable<T>> orderBy
     : Sorgu sonuçlarını sıralamak için kullanılır. q=> q.OrderBy(x => x.Name)
-    bool disableTracking : Sorgu sonuçlarının değişiklik izleme (change tracking) özelliğini devre dışı bırakmak için kullanılır.
+    bool enableTracking : Sorgu sonuçlarının değişiklik izleme (change tracking) özelliğini devre dışı bırakmak için kullanılır.
     */
 }
